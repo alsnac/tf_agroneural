2891c529734017e4f4d9bd50dd90034efdb6357a
Alisson A <alsnac@yahoo.com.br>
2021-11-10 20:14:57 -0300

    Update run_tf.sh

--- run_tf.sh
+++ run_tf.sh
@@ -52,6 +52,9 @@ cd $3
 #Treinamento
 python model_main_tf2.py --model_dir=$4 --pipeline_config_path="$4/pipeline.config"
 
+#renomear a pasta my_models para o nomde do modelo
+mv models/my_model "models/${model_name}"
+
 #Avaliação
 #python model_main_tf2.py --model_dir=$4 --pipeline_config_path="$4/pipeline.config" --checkpoint_dir=$4 --num_workers=1 --sample_1_of_n_eval_examples=1
 
