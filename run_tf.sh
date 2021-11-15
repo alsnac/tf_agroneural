723e983d76eb1bdf3d8fd196d847cbb351f510a6
Alisson A <alsnac@yahoo.com.br>
2021-11-15 10:12:57 -0300

    Update run_tf.sh

--- run_tf.sh
+++ run_tf.sh
@@ -56,13 +56,13 @@ python model_main_tf2.py --model_dir=$4 --pipeline_config_path="$4/pipeline.conf
 mv models/my_model "models/${model_name}"
 
 #dar permissão para todos
-chmod -r 777 "models/${model_name}"
+chmod -R 777 "models/${model_name}"
 
 #Exportar
 python exporter_main_v2.py --input_type image_tensor --pipeline_config_path="$4/pipeline.config" --trained_checkpoint_dir $4 --output_directory "$3/exported-models"
 
 #dar permissão para todos
-chmod -r 777 "$3/exported-models"
+chmod -R 777 "$3/exported-models"
 
 #Avaliação
 python model_main_tf2.py --model_dir=$4 --pipeline_config_path="$4/pipeline.config" --checkpoint_dir=$4 --num_workers=1 --sample_1_of_n_eval_examples=1
