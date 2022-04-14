a51ee217fa2a1aec1ec01d94a1699202ccae5e4c
Alisson A <alsnac@yahoo.com.br>
2022-04-14 10:20:20 -0300

    Update run_yolo.sh

--- run_yolo.sh
+++ run_yolo.sh
@@ -2,7 +2,7 @@
 #tail -f /dev/null
 #----------------------------------------------------------------------------------------------------------------------
 
-cd /usr/src/
+cd /usr/src/yolov5
 python train.py --img 1024 --batch 30 --data data.yaml --weights yolov5x6.pt --name Model #--resume runs/train/Model5/weights/last.pt
 
 #Modelos: yolov5s6  yolov5m6  yolov5l6  yolov5x6 (small, medium, large, extralarge)
