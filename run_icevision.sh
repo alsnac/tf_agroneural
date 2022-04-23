12f0cd9ff8f110b7397122f2e90e53fb77a6b5fd
Alisson A <alsnac@yahoo.com.br>
2022-04-23 14:30:47 -0300

    Update run_icevision.sh

--- run_icevision.sh
+++ run_icevision.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 nvidia-smi
 python -m pip install --upgrade pip
-python -m pip install setuptools==59.5.0
 python -m pip install ipywidgets
 python -m pip install tk-tools
+python -m pip install setuptools==59.5.0
 wget https://raw.githubusercontent.com/airctic/icevision/master/icevision_install.sh
 bash icevision_install.sh cuda10
 python resnext_run.py 
