cce5fdd2e390b79cd9d23b7367a75d1522cf36d4
Alisson A <alsnac@yahoo.com.br>
2022-04-23 14:18:48 -0300

    Update run_icevision.sh

--- run_icevision.sh
+++ run_icevision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python3 -m pip install --upgrade pip
+python -m pip install --upgrade pip
+python -m pip install setuptools==59.5.0
 wget https://raw.githubusercontent.com/airctic/icevision/master/icevision_install.sh
 bash icevision_install.sh cuda10
 python resnext_run.py 
