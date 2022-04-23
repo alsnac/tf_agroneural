970dc4e806ebbf4204b5065ed371edeced69eeda
Alisson A <alsnac@yahoo.com.br>
2022-04-23 12:59:26 -0300

    Update run_icevision.sh

--- run_icevision.sh
+++ run_icevision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pws
-ls -la
+ls -la test
+ls -la train
 wget https://raw.githubusercontent.com/airctic/icevision/master/icevision_install.sh
 bash icevision_install.sh cuda10
 python resnext_run.py 
