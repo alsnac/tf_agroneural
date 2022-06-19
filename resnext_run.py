1f3251a7d6748f3aee39b6ae076af5679790af97
Lucas Lima <lucaslimaneves1@hotmail.com>
2022-06-19 11:52:55 -0300

    Update resnext_run.py

--- resnext_run.py
+++ resnext_run.py
@@ -16,8 +16,8 @@ parser = parsers.VOCBBoxParser(
 train_records, valid_records = parser.parse(
     RandomSplitter([0.80, 0.20], seed=1))
 
-image_size = 1148
-presize = 1148
+image_size = 1024
+presize = 1024
 train_tfms = tfms.A.Adapter(
     [*tfms.A.aug_tfms(size=(image_size, image_size), presize=presize), tfms.A.Normalize()])
 valid_tfms = tfms.A.Adapter(
@@ -79,6 +79,6 @@ models.save_icevision_checkpoint(
     model_name='mmdet.vfnet', 
     backbone_name='resnext101_32x4d_fpn_mdconv_c3_c5_mstrain_2x',
     classes=parser.class_map.get_classes(), 
-    img_size=1148, 
+    img_size=1024, 
     filename='/agroneural_pragas/checkpoints/weights.pth'
 )
