c16ab5817b9abefc6e06c993e9027f58ce3bf2c6
Alisson A <alsnac@yahoo.com.br>
2022-07-22 23:20:04 -0300

    Update run_yolo.sh

--- run_yolo.sh
+++ run_yolo.sh
@@ -7,8 +7,8 @@ pip install opencv-python==4.5.5.64
 ls -la
 cd /usr/src/yolov5
 
-echo "30 de batch"
-python train.py --img 1024 --batch 30 --data data.yaml --weights yolov5l6.pt --name Model --workers 8 #--resume runs/train/Model5/weights/last.pt
+echo "60 de batch"
+python train.py --img 1024 --batch 60 --data data.yaml --weights yolov5l6.pt --name Model --workers 8 #--resume runs/train/Model5/weights/last.pt
 
 #Modelos: yolov5s6  yolov5m6  yolov5l6  yolov5x6 (small, medium, large, extralarge)
 #--cache: Mais rápido para treinar, mas enche a memória da GPU
