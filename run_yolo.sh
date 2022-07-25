124d2b6154b69934dff3ed8f8517170ab18072d8
Alisson A <alsnac@yahoo.com.br>
2022-07-25 07:51:38 -0300

    Update run_yolo.sh

--- run_yolo.sh
+++ run_yolo.sh
@@ -8,7 +8,13 @@ ls -la
 cd /usr/src/yolov5
 
 echo "50 de batch"
-python train.py --img 1024 --batch 50 --data data.yaml --weights yolov5l6.pt --name Model --workers 2 #--resume runs/train/Model5/weights/last.pt
+
+#A100
+#python train.py --img 1024 --batch 50 --data data.yaml --weights yolov5l6.pt --name Model --workers 2 #--resume runs/train/Model5/weights/last.pt
+
+
+#V100
+python train.py --img 1024 --batch 30 --data data.yaml --weights yolov5m6.pt --name Model #--resume runs/train/Model5/weights/last.pt
 
 #Modelos: yolov5s6  yolov5m6  yolov5l6  yolov5x6 (small, medium, large, extralarge)
 #--cache: Mais rápido para treinar, mas enche a memória da GPU
